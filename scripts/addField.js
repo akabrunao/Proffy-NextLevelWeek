b7974226b38de365a0ec5fc63fcb921f49c4b562
Bruno Rocha <brunoorochaa@gmail.com>
2020-08-05 21:45:40 -0300

    pagina dar aulas finalizada

---
+++ scripts/addField.js
@@ -0,0 +1,11 @@
+function cloneField() {
+  const newFieldContainer = document
+    .querySelector(".schedule-item")
+    .cloneNode(true);
+
+  const fields = newFieldContainer.querySelectorAll("input");
+  fields.forEach((field) => {
+    field.value = "";
+  });
+  document.querySelector("#schedule-items").appendChild(newFieldContainer);
+}
